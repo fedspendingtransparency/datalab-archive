6598d9562f6f73a6e882d5af038e19140d2b9fa8
Dylan <peinhardt_dylan@bah.com>
2018-01-17 12:42:11 -0500

    Added sql folder

---
+++ sql/tas-category-list.sql
@@ -0,0 +1,6 @@
+-- TAS Category List: https://api.usaspending.gov/api/v1/tas/categories/
+-- https://github.com/fedspendingtransparency/usaspending-api/blob/dev/usaspending_api/accounts/views/tas.py#L77
+SELECT * FROM financial_accounts_by_program_activity_object_class AS fab
+JOIN treasury_appropriation_account ta ON fab.treasury_account_id = ta.treasury_account_identifier
+JOIN ref_program_activity pa ON pa.id = fab.program_activity_id
+JOIN object_class oc ON oc.id = fab.object_class_id;
