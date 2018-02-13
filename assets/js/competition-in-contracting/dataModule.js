dc5a979ee87bb95ba987cf3b8bd141bbc6ad7ad1
Luke Michals <luke.michals@gmail.com>
2018-02-13 09:51:44 -0800

    Fix errors in Competition in Contracting
    - Fix data loading issue w/ Chrome 64
    - Fix styling of visualization

--- assets/js/competition-in-contracting/dataModule.js
+++ assets/js/competition-in-contracting/dataModule.js
@@ -2,11 +2,15 @@ const dataModule = (function() {
   const mem = {};
 
   function loadCicData(cb) {
-    d3.json("/data-lab-data/cicData.json", function(error, data) {
-      if (error) throw error;
-      mem.cicData = data;
-      cb(data);
-    });
+    d3.json(
+      "/data-lab-data/cicData.json",
+      function(error, data) {
+        if (error) throw error;
+        mem.cicData = data;
+        cb(data);
+      },
+      { credentials: "include" }
+    );
   }
 
   return {
