6ba2596cbcaf9283d087acbb8dc1352d65a99353
Matt-Murray <murray_matthew2@bah.com>
2018-02-22 10:12:43 -0500

    map panel 1 else color changed - homeless

--- assets/js/homeless.js
+++ assets/js/homeless.js
@@ -279,8 +279,10 @@ d3.json('/data-lab-data/2017_CoC_Grantee_Areas_2.json', function(us) {
                     return ('#365248');
                   } else if (table_data[i].total_homeless <= 12000) {
                     return ('#1E3D32');
+                  } else if (table_data[i].total_homeless <= 12000) {
+                    return ('#06281C');
                   } else {
-                    return ('#06281C')
+                    return ('#F2B400')
                   }
                 }
               }
@@ -1595,11 +1597,6 @@ d3.json('/data-lab-data/homeless_cluster.json', function(tree_data) {
         return color(d.parent.group);
       });
 
-    // cell.append("svg:text")
-    //   .attr("x",0)
-    //   .each(fontSize)
-    //   .each(wordWrap);
-
     cell.append("svg:text")
       .attr("x", function(d) {
         return d.dx / 2;
@@ -1724,13 +1721,6 @@ d3.json('/data-lab-data/homeless_cluster.json', function(tree_data) {
           return ky * d.dy - 1;
         })
 
-      // t.select("text")
-      //   .attr("x",0)
-      //   .attr("dx", "0.35em")
-      //   .attr("dy", "0.9em")
-      //   .each(fontSize)
-      //   .each(wordWrap);
-
       t.select("text")
         .attr("x", function(d) {
           return kx * d.dx / 2;
@@ -1770,82 +1760,6 @@ d3.json('/data-lab-data/homeless_cluster.json', function(tree_data) {
         '<p>Prevalence rate of alcohol dependece or abuse: ' + P3_formatNumber(dat.weighted_alcohol_dependence_or_abuse) + '%</p>'
       )
     }
-
-    // function fontSize(d,i) {
-    //   var size = d.dx/10;
-    //   console.log("fontSize: ",d)
-    //   var words = d.coc_name.split(' ');
-    //   var word = words[0];
-    //   var width = d.dx;
-    //   var height = d.dy;
-    //   var length = 0;
-    //   d3.select(this).style("font-size", size + "px").text(word);
-    //   while(((this.getBBox().width >= width) || (this.getBBox().height >= height)) && (size > 12))
-    //    {
-    //     size--;
-    //     d3.select(this).style("font-size", size + "px");
-    //     this.firstChild.data = word;
-    //    }
-    // }
-    //
-    // function wordWrap(d, i){
-    //   var words = d.coc_name.split(' ');
-    //   var line = new Array();
-    //   var length = 0;
-    //   var text = "";
-    //   var width = d.dx;
-    //   var height = d.dy;
-    //   var word;
-    //   do {
-    //      word = words.shift();
-    //      line.push(word);
-    //      if (words.length)
-    //        this.firstChild.data = line.join(' ') + " " + words[0];
-    //      else
-    //        this.firstChild.data = line.join(' ');
-    //      length = this.getBBox().width;
-    //      if (length < width && words.length) {
-    //        ;
-    //      }
-    //      else {
-    //        text = line.join(' ');
-    //        this.firstChild.data = text;
-    //        if (this.getBBox().width > width) {
-    //          text = d3.select(this).select(function() {return this.lastChild;}).text();
-    //          text = text + "...";
-    //          d3.select(this).select(function() {return this.lastChild;}).text(text);
-    //          d3.select(this).classed("wordwrapped", true);
-    //          break;
-    //       }
-    //       else
-    //         ;
-    //
-    //     if (text != '') {
-    //       d3.select(this).append("svg:tspan")
-    //       .attr("x", 0)
-    //       .attr("dx", "0.35em")
-    //       .attr("dy", "0.9em")
-    //       .text(text);
-    //     }
-    //     else
-    //        ;
-    //
-    //     if(this.getBBox().height > height && words.length) {
-    //        text = d3.select(this).select(function() {return this.lastChild;}).text();
-    //        text = text + "...";
-    //        d3.select(this).select(function() {return this.lastChild;}).text(text);
-    //        d3.select(this).classed("wordwrapped", true);
-    //
-    //        break;
-    //     }
-    //     else
-    //        ;
-    //
-    //     line = new Array();
-    //       }
-    //     } while (words.length);
-    //     this.firstChild.data = '';
-    // }
 
 
   })
