7b1b94bdd4365c304487b4fc0f1978a9e36b3512
Matt-Murray <murray_matthew2@bah.com>
2018-02-23 15:30:32 -0500

    dendro hover update

--- assets/js/dendrogram.js
+++ assets/js/dendrogram.js
@@ -453,7 +453,7 @@ function centerNode(source) {
                  .style('background','#ffffff')
                  .style('color','#333')
                  .style('border', 'solid 1px #BFBCBC')
-                 .style('padding', '15px')
+                 .style('padding', '15px 25px 15px 25px')
                  .style('min-width', '100px')
                  .style('max-width', '375px')
                  .offset([-10, -10])
@@ -519,21 +519,21 @@ function centerNode(source) {
              + d.name + '</p>' +
              '<p style="color: #0071BC; margin: 0; font-size: 18px">'+'Total Obligations: ' + formatNumber(d.size) + '</p>' +
              '<p style="margin: 0; font-size: 12px">'+'Unobligated Balance: ' + formatNumber(d.unob) + '</p>' +
-             '<p id="click" style="color: #000; font-size: 12px; font-style:italic; margin: 0 0 0 0;">' + 'Click to visit federal account page</p>';
+             '<br/><p id="click" style="color: #000; font-size: 12px; font-style:italic; margin: 0 0 0 0;">' + 'Click to visit federal account page</p>';
         }else if (d.depth === 2){
             return '<p style="border-bottom:1px solid #898C90; font-size: 14px; color: #000; font-weight:bolder;">'
             + d.name + ', ' +d.parent.name +  '</p>'
             + '<p style="color: #0071BC; margin: 0; font-size: 18px"><b style="color: #0071BC">'
             + 'Total Obligations: ' + sumUp_lvl2(d) + '</b></p>'
             + '<p style= "margin: 0; font-size: 12px">' + 'Unobligated Balance: '+ sumUp_lvl2_unob(d) +'</p>'
-            + '<p id="click" style="color: #000; font-size: 12px; font-style:italic; margin: 0 0 0 0;">'+ 'Click to view federal accounts'+ '</p>';
+            + '<br/><p id="click" style="color: #000; font-size: 12px; font-style:italic; margin: 0 0 0 0;">'+ 'Click to view federal accounts'+ '</p>';
         }else if (d.depth===1){
             return '<p style="border-bottom:1px solid #898C90; font-size: 14px; color: #000; font-weight:bolder;">'
             + d.name +  '</p>'
             + '<p style="color: #0071BC; margin: 0; font-size: 18px"><b style="color: #0071BC">'
             + 'Total Obligations: ' + sumUp(d) + '</b></p>'
             + '<p style= "margin: 0; font-size: 12px">' + 'Unobligated Balance: '+ sumUp_unob(d) + '</p>'
-            + '<p id="click" style="color: #000; font-size: 12px; font-style:italic; margin: 0 0 0 0;">' + 'Click to view agencies'+ '</p>';
+            + '<br/><p id="click" style="color: #000; font-size: 12px; font-style:italic; margin: 0 0 0 0;">' + 'Click to view agencies'+ '</p>';
         }else if (d.depth===0){
             return '<p style="font-size:16px; margin:0; color: #000; font-weight:bolder;">'
             + 'FY17 Federal Agencies'+ '</p>';
