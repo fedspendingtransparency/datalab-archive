42fd20e6fc5dcaa7b8f9e7b54ac67006944bbbfd
Luke Michals <luke.michals@gmail.com>
2018-03-14 11:55:50 -0700

    add linechart logic

--- assets/js/contracts-over-time/linechartModule.js
+++ assets/js/contracts-over-time/linechartModule.js
@@ -1,14 +1,22 @@
 const linechartModule = (function() {
   function draw(data) {
-    var margin = { top: 20, right: 20, bottom: 30, left: 80 },
+    // set the dimensions and margins of the graph
+    var margin = { top: 20, right: 50, bottom: 30, left: 100 },
       width = 800 - margin.left - margin.right,
       height = 800 - margin.top - margin.bottom;
 
-    var x = d3
-      .scaleBand()
-      .range([0, width])
-      .padding(0.1);
+    // parse the date / time
+    var parseTime = d3.timeParse("%Y-%m-%d");
+
+    // set the ranges
+    var x = d3.scaleTime().range([0, width]);
     var y = d3.scaleLinear().range([height, 0]);
+    var formatAsMillions = d3.format(".2s");
+
+    var valueline = d3
+      .line()
+      .x(d => x(d.parsedDate))
+      .y(d => y(d.contractdollars));
 
     var svg = d3
       .select("#svg-1")
@@ -16,36 +24,39 @@ const linechartModule = (function() {
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    var formatmillions = d3.format(".2s");
-    var formatnumber = d3.format(".2f");
 
-    data.forEach(d => (d.totalbyyear = +d.totalbyyear));
-    x.domain(data.map(d => d.fiscalyear));
-    y.domain([0, d3.max(data, d => d.totalbyyear)]);
+    // format the data
+    data.forEach(function(d) {
+      d.parsedDate = parseTime(d.date);
+      d.contractdollars = +d.contractdollars;
+    });
 
+    console.log(data.slice(0, 10));
+
+    // Scale the range of the data
+    x.domain(d3.extent(data, d => d.parsedDate));
+    y.domain([0, d3.max(data, d => d.contractdollars)]);
+
+    // Add the valueline path.
     svg
-      .selectAll(".bar")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("class", "bar")
-      .attr("x", d => x(d.fiscalyear))
-      .attr("width", x.bandwidth())
-      .attr("y", d => y(d.totalbyyear))
-      .attr("height", d => height - y(d.totalbyyear));
+      .append("path")
+      .data([data])
+      .attr("class", "line")
+      .attr("d", valueline);
 
+    // Add the X Axis
     svg
       .append("g")
-      .attr("class", "axis")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
+
+    // Add the Y Axis
     svg
       .append("g")
-      .attr("class", "axis")
       .call(
         d3
           .axisLeft(y)
-          .tickFormat(d => formatmillions(d).replace("G", " billion"))
+          .tickFormat(d => formatAsMillions(d).replace("G", " billion"))
       );
   }
 
