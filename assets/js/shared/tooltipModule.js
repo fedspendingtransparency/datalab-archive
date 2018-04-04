5662da213a36cb299af0a746915311f3ee673957
Luke Michals <luke.michals@gmail.com>
2018-04-04 15:37:33 -0700

    refactor for new style guide

--- assets/js/shared/tooltipModule.js
+++ assets/js/shared/tooltipModule.js
@@ -1,56 +1,55 @@
 ---
 ---
 
-const tooltipModule = (function() {
-  function draw(tooltipId, title, information) {
-    d3
-      .select(tooltipId)
-      .transition()
-      .duration(200)
-      .style("opacity", 1);
+window.tooltipModule = (() => {
+    function draw(tooltipId, title, information) {
+        d3
+            .select(tooltipId)
+            .transition()
+            .duration(200)
+            .style("opacity", 1);
 
-    d3
-      .select(tooltipId)
-      .html(toolTipHtml(title, information))
-      .style("left", `${d3.event.pageX}px`)
-      .style("top", `${d3.event.pageY}px`);
+        function toolTipHtml(t, i) {
+            const html = `
+        <p class="title">
+          <b>${t}</b>
+        </p>
+        <br>
+        <div class="information">
+          ${Object.entries(i).reduce((a, c) => {
+        a += `
+                <p class="key">${c[0]}</p>
+                <p class="val">${c[1]}</p>
+                    `;
+        return a;
+    }, "")}
+        <div />`;
 
-    function toolTipHtml(title, information) {
-      const html = `
-      <p class="title">
-        <b>${title}</b>
-      </p>
-      <br>
-      <div class="information">
-        ${Object.entries(information).reduce((a, c) => {
-          a += `
-            <p class="key">${c[0]}</p>
-            <p class="val">${c[1]}</p>
-            `;
-          return a;
-        }, "")}
-      <div />  
-      `;
+            return html;
+        }
 
-      return html;
+        d3
+            .select(tooltipId)
+            .html(toolTipHtml(title, information))
+            .style("left", `${d3.event.pageX}px`)
+            .style("top", `${d3.event.pageY}px`);
     }
-  }
 
-  function remove(tooltipId) {
-    d3
-      .select(tooltipId)
-      .transition()
-      .duration(500)
-      .style("opacity", 0)
-      .style("pointer-events", "none");
-  }
+    function remove(tooltipId) {
+        d3
+            .select(tooltipId)
+            .transition()
+            .duration(500)
+            .style("opacity", 0)
+            .style("pointer-events", "none");
+    }
 
-  function move(tooltipId) {
-    d3
-      .select(tooltipId)
-      .style("left", `${d3.event.pageX + 15}px`)
-      .style("top", `${d3.event.pageY + 15}px`);
-  }
+    function move(tooltipId) {
+        d3
+            .select(tooltipId)
+            .style("left", `${d3.event.pageX + 15}px`)
+            .style("top", `${d3.event.pageY + 15}px`);
+    }
 
-  return { draw, remove, move };
+    return { draw, remove, move };
 })();
