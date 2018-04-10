067624682d6fe2a51555c7304476b7b76c7dd022
Luke Michals <luke.michals@gmail.com>
2018-04-10 10:57:06 -0700

    Refactor for new style guide

--- assets/js/fedscope-tool/mapModule.js
+++ assets/js/fedscope-tool/mapModule.js
@@ -1,111 +1,110 @@
 ---
 ---
 
-const mapModule = (function() {
-  function draw(data, { states }) {
-    const svg = d3.select("#mapSvg")
-    svg.selectAll("*").remove();
+window.mapModule = {
+    draw: (data, { states }) => {
+        const { tooltipModule } = window;
 
-    let filteredData = [...data];
+        const svg = d3.select("#mapSvg");
+        svg.selectAll("*").remove();
 
-    const initialStateData = Object.keys(states).reduce((a, c) => {
-      a[c] = 0;
-      return a;
-    }, {});
+        const filteredData = [...data];
 
-    const dataByState = filteredData.reduce((a, c) => {
-      a[c.stateAbbreviation] = a[c.stateAbbreviation] + c.employeeCount;
-      return a;
-    }, initialStateData);
+        const initialStateData = Object.keys(states).reduce((a, c) => {
+            a[c] = 0;
+            return a;
+        }, {});
 
-    const max = d3.max(Object.values(dataByState), d => d);
-    const min = d3.min(Object.values(dataByState), d => d);
+        const dataByState = filteredData.reduce((a, c) => {
+            a[c.stateAbbreviation] += c.employeeCount;
+            return a;
+        }, initialStateData);
 
-    const formatNumber = d3.format(",d");
+        const max = d3.max(Object.values(dataByState), (d) => d);
+        const min = d3.min(Object.values(dataByState), (d) => d);
 
-    const colors = ["#f2f1f8", "#3f3c7d"];
+        const formatNumber = d3.format(",d");
 
-    var color = d3
-      .scaleLinear()
-      .domain([min, max])
-      .range(colors)
-      .interpolate(d3.interpolateRgb);
+        const colors = ["#f2f1f8", "#3f3c7d"];
 
-    function handleMouseOver(d) {
-      tooltipModule.draw("#tooltip", d.name, {
-        Employees: formatNumber(dataByState[d.abbreviation])
-      });
-      d3.select(this).style("fill", "#D334BA");
+        const color = d3
+            .scaleLinear()
+            .domain([min, max])
+            .range(colors)
+            .interpolate(d3.interpolateRgb);
+
+        function handleMouseOver(d) {
+            tooltipModule.draw("#tooltip", d.name, {
+                Employees: formatNumber(dataByState[d.abbreviation])
+            });
+            d3.select(this).style("fill", "#D334BA");
+        }
+
+        function handleMouseOut() {
+            tooltipModule.remove("#tooltip");
+            d3.select(this).style("fill", (d) => color(dataByState[d.abbreviation]));
+        }
+
+        function handleMouseMove() {
+            tooltipModule.move("#tooltip");
+        }
+
+        const mapGroup = svg
+            .append("g")
+            .attr("transform", "scale(.9375)");
+
+        mapGroup
+            .selectAll(".state")
+            .data(Object.values(states))
+            .enter()
+            .append("path")
+            .attr("class", "state")
+            .attr("d", (d) => d.path)
+            .style("fill", (d) => color(dataByState[d.abbreviation]))
+            .on("mouseover", handleMouseOver)
+            .on("mousemove", handleMouseMove)
+            .on("mouseout", handleMouseOut);
+
+        const svgWidth = 900;
+        const scaleWidth = 400;
+
+        const scaleGroup = svg
+            .append("g")
+            .attr("transform", `translate(${(svgWidth - scaleWidth) / 2}, 10)`);
+
+        const linearGradient = scaleGroup
+            .append("defs")
+            .append("linearGradient")
+            .attr("id", "linear-gradient");
+
+        linearGradient
+            .append("stop")
+            .attr("offset", "0%")
+            .attr("stop-color", colors[0]);
+
+        linearGradient
+            .append("stop")
+            .attr("offset", "100%")
+            .attr("stop-color", colors[1]);
+
+        scaleGroup
+            .append("rect")
+            .attr("height", 10)
+            .attr("width", scaleWidth)
+            .attr("fill", "url(#linear-gradient)");
+
+        scaleGroup
+            .append("text")
+            .text(`${formatNumber(min)}`)
+            .attr("transform", `translate(0, 20)`)
+            .attr("text-anchor", "middle")
+            .attr("font-size", 12);
+
+        scaleGroup
+            .append("text")
+            .text(`${formatNumber(max)}`)
+            .attr("transform", `translate(${scaleWidth}, 20)`)
+            .attr("text-anchor", "middle")
+            .attr("font-size", 12);
     }
-
-    function handleMouseOut() {
-      tooltipModule.remove("#tooltip");
-      d3.select(this).style("fill", d => color(dataByState[d.abbreviation]));
-    }
-
-    function handleMouseMove() {
-      tooltipModule.move("#tooltip");
-    }
-
-    const mapGroup = svg
-      .append("g")
-      .attr("transform", "scale(.9375)");
-    
-    mapGroup
-      .selectAll(".state")
-      .data(Object.values(states))
-      .enter()
-      .append("path")
-      .attr("class", "state")
-      .attr("d", d => d.path)
-      .style("fill", d => color(dataByState[d.abbreviation]))
-      .on("mouseover", handleMouseOver)
-      .on("mousemove", handleMouseMove)
-      .on("mouseout", handleMouseOut);
-
-    const svgWidth = 900;
-    const scaleWidth = 400;
-
-    const scaleGroup = svg
-      .append("g")
-      .attr("transform", `translate(${(svgWidth - scaleWidth)/2}, 10)`)
-
-    const linearGradient = scaleGroup
-      .append("defs")
-      .append("linearGradient")
-      .attr("id", "linear-gradient");
-
-    linearGradient
-      .append("stop")
-      .attr("offset", "0%")
-      .attr("stop-color", colors[0]);
-  
-    linearGradient
-      .append("stop") 
-      .attr("offset", "100%")   
-      .attr("stop-color", colors[1]);
-
-    scaleGroup
-      .append("rect")
-      .attr("height", 10)
-      .attr("width", scaleWidth)
-      .attr("fill", "url(#linear-gradient)");
-    
-    scaleGroup
-      .append("text")
-      .text(`${formatNumber(min)}`)
-      .attr("transform", `translate(0, 20)`)
-      .attr("text-anchor", "middle")
-      .attr("font-size", 12)
-      
-    scaleGroup
-      .append("text")
-      .text(`${formatNumber(max)}`)
-      .attr("transform", `translate(${scaleWidth}, 20)`)
-      .attr("text-anchor", "middle")
-      .attr("font-size", 12)
-
-
-  }
-  return { draw };
-})();
+};
--- assets/js/fedscope-tool/treemapModule.js
+++ assets/js/fedscope-tool/treemapModule.js
@@ -1,76 +1,74 @@
 ---
 ---
 
-const treemapModule = (function() {
-  var svg = d3.select("#treemapSvg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+window.treemapModule = {
+    draw: (data, { agencies }) => {
+        const svg = d3.select("#treemapSvg");
+        const width = +svg.attr("width");
+        const height = +svg.attr("height");
 
-  const format = d3.format(",d");
+        const { tooltipModule } = window;
 
-  var treemap = d3
-    .treemap()
-    .tile(d3.treemapResquarify)
-    .size([width, height])
-    .round(true)
-    .paddingInner(1);
+        const treemap = d3
+            .treemap()
+            .tile(d3.treemapResquarify)
+            .size([width, height])
+            .round(true)
+            .paddingInner(1);
 
-  function draw(data, { agencies }) {
-    var root = d3
-      .hierarchy(data)
-      .sum(d => d.obligations)
-      .sort((a, b) => b.height - a.height || b.value - a.value);
+        function handleMouseOver(d) {
+            const formatNumber = d3.format("$,d");
+            tooltipModule.draw("#tooltip", agencies[d.data.agencyId].name, {
+                "Employee Salaries": formatNumber(d.value)
+            });
+            d3.select(this).style("fill", "#D334BA");
+        }
 
-    treemap(root);
+        function handleMouseOut() {
+            tooltipModule.remove("#tooltip");
+            d3.select(this).style("fill", "#6E9BA3");
+        }
 
-    var cell = svg
-      .selectAll("g")
-      .data(root.leaves())
-      .enter()
-      .append("g")
-      .attr("transform", d => `translate(${d.x0},${d.y0})`);
+        function handleMouseMove() {
+            tooltipModule.move("#tooltip");
+        }
 
-    cell
-      .append("rect")
-      .attr("id", d => d.data.agencyId)
-      .attr("width", d => d.x1 - d.x0)
-      .attr("height", d => d.y1 - d.y0)
-      .attr("fill", "#6E9BA3")
-      .on("mouseover", handleMouseOver)
-      .on("mousemove", handleMouseMove)
-      .on("mouseout", handleMouseOut);
+        const root = d3
+            .hierarchy(data)
+            .sum((d) => d.obligations)
+            .sort((a, b) => b.height - a.height || b.value - a.value);
 
-    cell
-      .append("clipPath")
-      .attr("id", d => `clip-${d.data.agencyId}`)
-      .append("use")
-      .attr("xlink:href", d => `#${d.data.agencyId}`);
+        treemap(root);
 
-    cell
-      .append("text")
-      .text(d => agencies[d.data.agencyId].abbreviation)
-      .attr("dy", 12)
-      .attr("dx", 2)
-      .attr("fill", "white")
-      .attr("pointer-events", "none");
+        const cell = svg
+            .selectAll("g")
+            .data(root.leaves())
+            .enter()
+            .append("g")
+            .attr("transform", (d) => `translate(${d.x0},${d.y0})`);
 
-    function handleMouseOver(d) {
-      const formatNumber = d3.format("$,d");
-      tooltipModule.draw("#tooltip", agencies[d.data.agencyId].name, {
-        "Employee Salaries": formatNumber(d.value)
-      });
-      d3.select(this).style("fill", "#D334BA");
+        cell
+            .append("rect")
+            .attr("id", (d) => d.data.agencyId)
+            .attr("width", (d) => d.x1 - d.x0)
+            .attr("height", (d) => d.y1 - d.y0)
+            .attr("fill", "#6E9BA3")
+            .on("mouseover", handleMouseOver)
+            .on("mousemove", handleMouseMove)
+            .on("mouseout", handleMouseOut);
+
+        cell
+            .append("clipPath")
+            .attr("id", (d) => `clip-${d.data.agencyId}`)
+            .append("use")
+            .attr("xlink:href", (d) => `#${d.data.agencyId}`);
+
+        cell
+            .append("text")
+            .text((d) => agencies[d.data.agencyId].abbreviation)
+            .attr("dy", 12)
+            .attr("dx", 2)
+            .attr("fill", "white")
+            .attr("pointer-events", "none");
     }
-
-    function handleMouseOut() {
-      tooltipModule.remove("#tooltip");
-      d3.select(this).style("fill", "#6E9BA3");
-    }
-
-    function handleMouseMove() {
-      tooltipModule.move("#tooltip");
-    }
-  }
-
-  return { draw };
-})();
+};
