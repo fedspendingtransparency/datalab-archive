950a55614bcf5e758ff56505dcdd3605dd204d8b
Matt-Murray <murray_matthew2@bah.com>
2018-05-02 16:27:41 -0400

    CoT update
    legend hover in context enabled

--- assets/js/contracts-over-time/multiLinechartModule.js
+++ assets/js/contracts-over-time/multiLinechartModule.js
@@ -320,24 +320,38 @@ const multiLinechartModule = (function() {
           if(d === "Contract Modification"){
             d3.select("#svg-1 > g > g > g.line-paths > path:nth-child(1)").style("stroke-width","1px");
             d3.select("#svg-1 > g > g > g.line-paths > path:nth-child(2)").style("stroke-width","0px");
+            d3.select("#svg-1 > g > g.context > g.line-paths > path:nth-child(1)").style("stroke-width","1px");
+            d3.select("#svg-1 > g > g.context > g.line-paths > path:nth-child(2)").style("stroke-width","0px");
           }else if (d === "New Contract"){
             d3.select("#svg-1 > g > g > g.line-paths > path:nth-child(1)").style("stroke-width","0px");
             d3.select("#svg-1 > g > g > g.line-paths > path:nth-child(2)").style("stroke-width","1px");
+            d3.select("#svg-1 > g > g.context > g.line-paths > path:nth-child(1)").style("stroke-width","0px");
+            d3.select("#svg-1 > g > g.context > g.line-paths > path:nth-child(2)").style("stroke-width","1px");
           }else if (d === "Equipment/Facilities/Construction/Vehicles"){
             d3.selectAll("#svg-1 > g > g > g.line-paths > path").style("stroke-width","0px");
             d3.select("#svg-1 > g > g > g.line-paths > path:nth-child(1)").style("stroke-width","1px");
+            d3.selectAll("#svg-1 > g > g.context > g.line-paths > path").style("stroke-width","0px");
+            d3.select("#svg-1 > g > g.context > g.line-paths > path:nth-child(1)").style("stroke-width","1px");
           }else if (d === "Miscellaneous"){
             d3.selectAll("#svg-1 > g > g > g.line-paths > path").style("stroke-width","0px");
             d3.select("#svg-1 > g > g > g.line-paths > path:nth-child(2)").style("stroke-width","1px");
+            d3.selectAll("#svg-1 > g > g.context > g.line-paths > path").style("stroke-width","0px");
+            d3.select("#svg-1 > g > g.context > g.line-paths > path:nth-child(2)").style("stroke-width","1px");
           }else if (d === "Professional Services"){
             d3.selectAll("#svg-1 > g > g > g.line-paths > path").style("stroke-width","0px");
             d3.select("#svg-1 > g > g > g.line-paths > path:nth-child(3)").style("stroke-width","1px");
+            d3.selectAll("#svg-1 > g > g.context > g.line-paths > path").style("stroke-width","0px");
+            d3.select("#svg-1 > g > g.context > g.line-paths > path:nth-child(3)").style("stroke-width","1px");
           }else if (d === "Telecomm & IT"){
             d3.selectAll("#svg-1 > g > g > g.line-paths > path").style("stroke-width","0px");
             d3.select("#svg-1 > g > g > g.line-paths > path:nth-child(4)").style("stroke-width","1px");
+            d3.selectAll("#svg-1 > g > g.context > g.line-paths > path").style("stroke-width","0px");
+            d3.select("#svg-1 > g > g.context > g.line-paths > path:nth-child(4)").style("stroke-width","1px");
           }else if (d === "Weapons"){
             d3.selectAll("#svg-1 > g > g > g.line-paths > path").style("stroke-width","0px");
             d3.select("#svg-1 > g > g > g.line-paths > path:nth-child(5)").style("stroke-width","1px");
+            d3.selectAll("#svg-1 > g > g.context > g.line-paths > path").style("stroke-width","0px");
+            d3.select("#svg-1 > g > g.context > g.line-paths > path:nth-child(5)").style("stroke-width","1px");
           }
         })
         .on("mouseout",() => d3.selectAll("#svg-1 > g > g > g.line-paths > path").style("stroke-width","1px"));
