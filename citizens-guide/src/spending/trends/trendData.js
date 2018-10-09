3c6cb7079e2c3286ba99b603c8fc0ef9cb3c02db
matthewbrown314 <matthew.brown@kc.frb.org>
2018-10-09 15:24:38 -0500

    CG-358   - Create a first draft of Spending trends over time.   - Added a link to the Trends section in Spending Chapter   - Adjusted field names for the spending data vs the income data from the income trends over time.   - Made adjustments to package.json and webpack.config to pull in necessary files and to fix issues with npm install.

---
+++ citizens-guide/src/spending/trends/trendData.js
@@ -0,0 +1,44 @@
+import CategoryData from '../../../public/csv/spending_categories.csv';
+
+export function trendData(){
+    const indexed = {};
+
+    let arr;
+
+    CategoryData.forEach(r => {
+        if (isNaN(r.spending)) {
+            return;
+        }
+        
+        indexed[r.parent_plain] = indexed[r.parent_plain] || {
+            name: r.parent_plain,
+            officialName: r.parent,
+            values: [],
+            subcategories: {}
+        };
+
+        if (r.child) {
+            indexed[r.parent_plain].subcategories[r.child_plain] = indexed[r.parent_plain].subcategories[r.child_plain] || {
+                name: r.child_plain,
+                officialName: r.child,
+                values: [],
+            };
+
+            indexed[r.parent_plain].subcategories[r.child_plain].values.push({
+                year: r.fiscal_year,
+                amount: r.spending
+            })
+        } else {
+            indexed[r.parent_plain].values.push({
+                year: r.fiscal_year,
+                amount: r.spending
+            })
+        }
+    });
+
+    return Object.keys(indexed).map(c => {
+        indexed[c].subcategories = Object.keys(indexed[c].subcategories).map(s => indexed[c].subcategories[s]);
+        
+        return indexed[c];
+    })
+}
