c1a5f7bdfc626a1fdbeb769c5603f2e5f8962db9
matthewbrown314 <matthew.brown@kc.frb.org>
2018-11-19 08:40:52 -0600

    CG-563
     - Added in logic for filling out the anecdote module with the html that is provided.
     - Added in functionality for anecdote pane navigation.
     - Added in css to match the mockups that Jong provided.

--- citizens-guide/src/anecdote/anecdote.js
+++ citizens-guide/src/anecdote/anecdote.js
@@ -1,14 +1,82 @@
 import {select, selectAll} from 'd3-selection';
 
 
-const d3 = {select, selectAll};
+const d3 = {select, selectAll},
+    closeButtonClass = 'info-box__close';
+let dotLength = 0;
+
+function addCloseIcon(anecdote) {
+    const closeButton = anecdote.insert('button').classed(closeButtonClass, true).lower(),
+        closeIcon = closeButton.append('i').classed('fas fa-times', true);
+}
+
+function setActiveDot(anecdote, index){
+    const dotContainer = anecdote.selectAll('.anecdote__dot');
+    const activeDotClass = 'anecdote__dot--active';
+    dotContainer.classed(activeDotClass, false);
+    dotContainer.filter((d,i) => i === index).classed(activeDotClass,true);
+}
+
+function updateDotText(anecdote, dotLength, i){
+    const dotText = anecdote.select('.anecdote__dots--text');
+    dotText.html(`${i} of ${dotLength}`);
+}
+
+function buildDots(anecdote){
+    dotLength = anecdote.selectAll('.anecdote__pane').size();
+    const dotContainer = anecdote.select('.anecdote__contents').insert('div', '.anecdote__cta').classed('anecdote__dots--container', true);
+    const dots = dotContainer.append('div').classed('anecdote__dots', true);
+    const dotArray = new Array(dotLength);
+    dots.selectAll('.anecdote__dot')
+        .data(dotArray)
+        .enter()
+        .append('div')
+        .classed('anecdote__dot', true)
+        .on('click', function(d,i){
+            setActiveDot(anecdote, i);
+            showPane(anecdote, i);
+            updateDotText(anecdote, dotLength, i + 1);
+        });
+    dotContainer.append('div')
+        .classed('anecdote__dots--text', true);
+}
+
+function setPaneHeight(anecdote) {
+    const panes = anecdote.selectAll('.anecdote__pane');
+    let maxPaneHeight = 0;
+    let paneHeightStr = '';
+
+    panes.each(function () {
+        const myHeight = this.getBoundingClientRect().height;
+        maxPaneHeight = (myHeight > maxPaneHeight) ? myHeight : maxPaneHeight;
+    });
+
+    paneHeightStr = Math.ceil(maxPaneHeight) + 'px';
+
+    anecdote.select('.anecdote__panes')
+        .attr('style', `height: ${paneHeightStr}`);
+}
+
+function showPane(anecdote, index){
+    const panes = anecdote.selectAll('.anecdote__pane');
+    const activePaneClass = 'anecdote__pane--active';
+
+
+    panes.classed(activePaneClass, false);
+    panes.filter((d, i) => {return i === index}).classed(activePaneClass, true);
+}
 
 function toggleContent(anecdote){
-  const anecdotePanes = anecdote.select('anecdote__panes');
   const anecdoteContents = anecdote.select('.anecdote__contents');
   const activeInd = anecdoteContents.classed('anecdote__content--active');
-  anecdotePanes.classed('anecdote__pane', !activeInd);
   anecdoteContents.classed('anecdote__content--active', !activeInd);
+  if(!activeInd) {
+      setPaneHeight(anecdote);
+      anecdote.select('.' + closeButtonClass)
+          .on('click', function () {
+              toggleContent(anecdote);
+          });
+  }
 }
 
 function buildTrigger(anecdote){
@@ -21,19 +89,22 @@ function buildTrigger(anecdote){
 
 function wrapContents(anecdote){
     const html = anecdote.node().innerHTML;
-    let anecdotePane;
     let content;
 
     anecdote.selectAll('*').remove();
-    anecdotePane = anecdote.append("div").classed("anecdote__panes", true);
-    content = anecdotePane.append("div").classed("anecdote__contents", true);
+    content = anecdote.append("div").classed("anecdote__contents", true);
     content.html(html);
 }
 
 function buildAnecdote(){
     const anecdote = d3.select(this);
+    addCloseIcon(anecdote);
     wrapContents(anecdote);
     buildTrigger(anecdote);
+    showPane(anecdote, 0);
+    buildDots(anecdote);
+    setActiveDot(anecdote, 0);
+    updateDotText(anecdote, dotLength, 1)
 }
 
 function init(){
