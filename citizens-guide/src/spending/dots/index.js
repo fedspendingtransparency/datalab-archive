2d1cde5860ddd8cd1057662d9aed8686fd5e0e17
Brett Francis <brett.francis@kc.frb.org>
2018-11-30 08:46:34 -0600

    center the legend

--- citizens-guide/src/spending/dots/index.js
+++ citizens-guide/src/spending/dots/index.js
@@ -1,4 +1,17 @@
+import { select, selectAll } from 'd3-selection';
+import { establishContainer } from '../../utils';
 import './dots.scss';
 import { placeDots } from "./placeDots";
+import { startLegendAnimation } from './legend';
 
-placeDots();
+const d3 = { select, selectAll };
+
+function init() {
+    const width = d3.select('#viz').node().getBoundingClientRect().width;
+    
+    establishContainer(500, width);
+
+    startLegendAnimation(width);
+}
+
+init();
--- citizens-guide/src/spending/dots/placeDots.js
+++ citizens-guide/src/spending/dots/placeDots.js
@@ -1,15 +1,14 @@
 import { select, selectAll } from 'd3-selection';
+import { dotConstants } from './dotConstants';
 import { establishContainer } from "../../utils";
+import { placeOverlay } from './overlayRevenue';
 
 const d3 = { select, selectAll };
 
-const dotOffset = {
-    x: 5,
-    y: 6
-};
-
 let svg,
-    width;
+    width,
+    rowCount,
+    dotsPerRow;
 
 function dotFactory(container, x, y, color) {
     color = color || 'blue';
@@ -21,10 +20,40 @@ function dotFactory(container, x, y, color) {
         .style('fill', color)
 }
 
+function setDotsPerRow() {
+    const workingWidth = width - dotConstants.radius;
+
+    dotsPerRow = Math.floor(workingWidth / dotConstants.offset.x);
+}
+
+function readyDots() {
+    const dotContainer = svg.append('g');
+        // .attr('opacity', 0)
+        // .classed(receiptsConstants.incomeContainerClass, true)
+        // .attr('transform', 'translate(-100, -50), scale(2)');
+
+    let i = 0,
+        top = 4000,
+        x = dotConstants.radius,
+        y = 2;
+
+    for (i; i < top; i++) {
+        dotFactory(dotContainer, x, y);
+        x += dotConstants.offset.x;
+
+        if ((i + 1) % dotsPerRow === 0) {
+            y += dotConstants.offset.y;
+            x = dotConstants.radius;
+        }
+    }
+}
+
 export function placeDots() {
     width = d3.select('#viz').node().getBoundingClientRect().width;
 
-    console.log(width)
-    
     svg = establishContainer(500, width);
+
+    setDotsPerRow();
+    readyDots();
+    placeOverlay(dotsPerRow);
 }
