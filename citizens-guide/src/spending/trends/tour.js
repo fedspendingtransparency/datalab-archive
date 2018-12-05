cae0eb1b12504d113b07acc46cfae0f5249b42c0
Matthew Brown <matthew.brown@kc.frb.org>
2018-12-05 10:29:22 -0600

    Pushing spending->trends up to dev for review.

--- citizens-guide/src/spending/trends/tour.js
+++ citizens-guide/src/spending/trends/tour.js
@@ -1,5 +1,25 @@
-import { selectAll } from 'd3-selection';
+import { select, selectAll } from 'd3-selection';
 
-const d3 = { selectAll },
-    tour = location.search.indexOf('tour') !== -1,
-    factBox = d3.selectAll('.fact-box');
+const d3 = { select, selectAll },
+    disabledClass = 'tour__link--disabled';
+
+function disableButton(e) {
+    e.preventDefault();
+}
+
+export function initTwoPartTour() {
+    const linkButton = d3.select('a.tour__link');
+
+    linkButton.node().addEventListener('click', disableButton);
+    linkButton.classed(disabledClass, true)
+}
+
+export function activateTourPartTwo() {
+    const linkButton = d3.select('a.tour__link');
+
+    d3.select('.tour__part-two').classed('active', true);
+    d3.select('.tour__part-one').remove();
+
+    linkButton.node().removeEventListener('click', disableButton);
+    linkButton.classed(disabledClass, null)
+}
