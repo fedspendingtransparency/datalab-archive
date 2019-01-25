33749939f516c57a09563f788aa3bf668fb990ab
Brett Francis <brett.francis@kc.frb.org>
2019-01-25 13:57:23 -0600

    prep debt country comparison

---
+++ citizens-guide/src/debt/countries/index.js
@@ -0,0 +1,37 @@
+import '../../revenue/countries/selectCountry.scss';
+import { loadSourceData } from '../../revenue/countries/data';
+import CountryData from '../../../public/csv/surplus_deficit_gdp_by_country.csv';
+import { chartInit } from '../../revenue/countries/chart';
+
+const spendingConfig = {
+    amountField: 'surplus_deficit',
+    gdpField: 'surplus_deficit_gdp',
+    amountLabel: 'Deficit',
+    sourceFields: ['spending_source', 'gdp_source'],
+    chapter: 'deficit',
+    defaultCountries: [{
+        display: 'United States',
+        plainName: 'United States'
+    },{
+        display: 'China',
+        plainName: 'China'
+    },{
+        display: 'Japan',
+        plainName: 'Japan'
+    },{
+        display: 'Germany',
+        plainName: 'Germany'
+    },{
+        display: 'United Kingdom',
+        plainName: 'United Kingdom'
+    },{
+        display: 'India',
+        plainName: 'India'
+    },{
+        display: 'France',
+        plainName: 'France'
+    }]
+};
+
+loadSourceData(CountryData);
+chartInit(spendingConfig);
