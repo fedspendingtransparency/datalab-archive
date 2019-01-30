aa566e49899d8501de33471b499f9cffc2a0beea
Matthew Brown <matthew.brown@kc.frb.org>
2019-01-30 14:20:50 -0600

    Added search capabilities, added ability to open glossary to term based on actionable text in the document, fixed SVGs not showing.

--- citizens-guide/src/components/glossary/glossary.js
+++ citizens-guide/src/components/glossary/glossary.js
@@ -1,10 +1,15 @@
 import glossaryData from '../../../../assets/ffg/components/glossary/glossary.csv';
+import { select, selectAll } from 'd3-selection';
 
-function categorizeGlossaryData(){
+const d3 = { select, selectAll };
+
+let origDategorizedTerms = [];
+
+function categorizeGlossaryData(data){
     const categorizedData = [];
     let hashChar = '';
 
-    glossaryData.forEach(function(d){
+    data.forEach(function(d){
         hashChar = d.term.substring(0,1);
         if(!categorizedData[hashChar]){
             categorizedData[hashChar] = [];
@@ -15,11 +20,44 @@ function categorizeGlossaryData(){
     return categorizedData;
 }
 
-function createTermHTMLElements(terms){
+function createSVGElements(){
+    const closeButtonSvg = d3.select('#cg-glossary-close-button').append('svg')
+        .classed('cg-icon-close', true)
+        .attr('viewBox', '0 0 512 512')
+         .attr('aria-label', 'Close Glossary 123'),
+        searchBox = d3.select('#cg-search-button'),
+        searchBoxSvg = searchBox.append('svg')
+            .classed('cg-icon-search', true)
+            .attr('viewBox', '0 0 512 512')
+            .attr('aria-label', 'search'),
+        backButtonSvg = d3.select('.cg-glossary-back__content').append('svg').classed('cg-glossary-angle-left', true)
+            .attr('viewBox', '0 0 512 512')
+            .attr('aria-label', 'Back');
+
+
+    console.log('searchBoxSvg:', searchBoxSvg);
+    closeButtonSvg.append('title')
+        .text('Close Glossary');
+    closeButtonSvg.append('g').append('path')
+        .attr('d', 'M256 212L50 6 6 50l206 206L6 462l44 44 206-206 206 206 44-44-206-206L506 50 462 6');
+    searchBoxSvg.append('title')
+        .text('Search');
+    searchBoxSvg.append('g').append('path')
+        .attr('d', 'M337.8 208.3c0-36.5-12-66.8-37.4-92-25.3-25.4-55.7-37.6-92-37.6-35.5 0-66 12.2-91.2 37.5-25.3 25.3-38.5 55.6-38.5 92 0 35.5 13.2 66 38.5 91.2 25.3 25.3 55.7 38.4 91 38.4 36.5 0 67-13 92.2-38.4 25.3-25.3 37.4-55.7 37.4-91zM453.5 506L327 379.5c-34.6 24.4-73.3 36.7-116 36.7-27.6 0-54-5-79.6-16.3-25.5-12-47-26-65.3-44-18-19-32-40-43-66-11-25-17-52-17-79 0-29 6-55 16.8-80C33.8 106 48 83.2 66 65c18.6-18.3 40-32.6 65.5-44 25.5-9.8 52-15 79.5-15 28.4 0 55 5 80.4 15.3 24.5 11.2 47 25.5 65.3 44 18.3 18.2 32.6 40.7 43.8 66.2 10.2 24.5 16.3 51 16.3 79.6 0 43-12.2 82-36.7 117l126 126-52 53z');
+    backButtonSvg.append('title')
+        .text('Back');
+    backButtonSvg.append('g').append('path')
+        .attr('d', 'M368.5 77.2L208 255l160.2 179.7.4 71.3-225.2-250.5L368 6');
+}
+function createTermHTMLElements(terms, searchTerm){
+    console.log(terms, searchTerm);
     const termListDiv = $('#cg-glossary-term-list-div'),
         termResultList = termListDiv.find('.cg-glossary-search-results');
 
-        let hashSection = null,
+        termResultList.empty();
+
+        let filteredTerms = {},
+            hashSection = null,
             hashTitle,
             hashDivider,
             groupItems,
@@ -27,42 +65,69 @@ function createTermHTMLElements(terms){
             term,
             termButton;
 
-       Object.keys(terms).forEach(function(t){
-           termGroup = terms[t];
-           console.log(t, termGroup);
+        if(searchTerm){
+            console.log(searchTerm);
+            filteredTerms = glossaryData.filter(function(t){
+                const upperCaseSearchTerm = searchTerm.toUpperCase();
+                return t.term.toUpperCase().match(upperCaseSearchTerm);
+            });
+            filteredTerms = categorizeGlossaryData(filteredTerms);
+        } else {
+            filteredTerms = origDategorizedTerms;
+        }
 
-           hashSection = document.createElement('div');
-           hashSection.className = 'cg-glossary-result-group';
+        console.log('filteredTerms:', filteredTerms);
 
-           hashTitle = document.createElement('h2');
-           hashTitle.className = 'cg-group-title';
-           hashTitle.innerText = t;
+       Object.keys(filteredTerms).forEach(function(t){
+           termGroup = filteredTerms[t];
 
-           hashDivider = document.createElement('hr');
-           hashDivider.className = 'cg-group-divider';
+           hashSection = $('<div class="cg-glossary-result-group"></div>');
 
-           groupItems = document.createElement('ul');
-           groupItems.className = 'cg-group-items';
+           hashTitle = $(`<h2 class="cg-group-title">${t}</h2>`);
+
+           hashDivider = $('<hr class="cg-group-divider"/>');
+
+           groupItems = $('<ul class="cg-group-items"></ul>');
 
            termResultList.append(hashSection);
-           hashSection.appendChild(hashTitle);
-           hashSection.appendChild(hashDivider);
-           hashSection.appendChild(groupItems);
+           hashSection.append(hashTitle,hashDivider,groupItems);
 
            termGroup.forEach(function(t1){
-               term = document.createElement('li');
-               termButton = document.createElement('button');
-               termButton.setAttribute('hashMap', t);
-               termButton.className = 'cg-glossary-link';
-               termButton.innerText = t1.term;
+               term = $('<li></li>');
+               termButton = $(`<button hashMap="${t}" class="cg-glossary-link">${t1.term}</button>`);
 
-               term.appendChild(termButton);
-               groupItems.appendChild(term);
+               term.append(termButton);
+               groupItems.append(term);
            })
         });
 }
 
-function showIndividualTerm(definitionWrapper, term){
+function showIndividualTerm(termsArr, termDisplay){
+    const definitionWrapperId = '#cg-definition-wrapper',
+        definitionWrapper = $(definitionWrapperId),
+        showListResultsInd = false;
+
+    let term = {};
+
+    if(termsArr && termDisplay){
+        /*
+         * It may seem like a fragile idea to use the term display to perform the filter considering the csv has ids.
+         * The problem is that if a term is added/removed to the glossary.csv, this will change the ids on all of the terms
+         * following the term that changed. When we have the ability to click on text in the document to launch the glossary and view
+         * that term, that means the ids need to be placed on all of the actionable text in the document AND that any updates to the
+         * glossary means updating the ids on all actionable text pieces throughout CG (an absolute nightmare).
+         */
+        console.log(termDisplay, termsArr);
+        term = termsArr.filter(function(d){
+            return d.term.toUpperCase() === termDisplay.toUpperCase();
+        });
+        if(!term.length){
+            return;
+        }
+        term = term[0];
+    }
+
+    definitionWrapper.empty();
     definitionWrapper.append(`<h2 class="cg-glossary-term">${term.term}</h2>`);
 
     const definition = term.definition;
@@ -80,22 +145,33 @@ function showIndividualTerm(definitionWrapper, term){
         definitionWrapper.append(`<div class="cg-glossary-resources"><h3 class="cg-glossary-resources-title">More Resources</h3>`,
             `<hr><div><p class="cg-glossary-resources-text">Learn More: <a href="${term.url_path}" target="_blank" rel="noopener noreferrer">${term.url_display}</a></p></div>`);
     }
+    setTermListView(showListResultsInd);
 }
 
-function toggleActiveStatus(glossaryWrapper) {
+function setActiveStatus(glossaryWrapper, activeInd) {
     const openClass = 'active';
 
-    glossaryWrapper.toggleClass(openClass);
+    if(activeInd){
+        glossaryWrapper.addClass(openClass);
+    } else {
+        glossaryWrapper.removeClass(openClass);
+    }
 }
 
-function toggleTermListView(){
+function setTermListView(showListResultsInd){
     const searchResultsClass = '.cg-glossary-search-results',
         glossaryDefinitionClass = '.cg-glossary-definition',
         searchResultsDiv = $(searchResultsClass),
-        glossaryDefinitionDiv = $(glossaryDefinitionClass);
+        glossaryDefinitionDiv = $(glossaryDefinitionClass)
 
-    searchResultsDiv.toggleClass('hidden');
-    glossaryDefinitionDiv.toggleClass('hidden');
+    if(showListResultsInd){
+        searchResultsDiv.removeClass('hidden');
+        glossaryDefinitionDiv.addClass('hidden');
+    } else {
+
+        searchResultsDiv.addClass('hidden');
+        glossaryDefinitionDiv.removeClass('hidden');
+    }
 }
 
 function resizeTermListDiv(){
@@ -109,56 +185,98 @@ function resizeTermListDiv(){
     }
 }
 
+function addGlossaryEvents(glossaryWrapper, glossaryButton, terms){
+    const searchForm = $('.cg-glossary-search-bar form'),
+        searchTextBox = $('#cg-search-text-box'),
+        actionableTextEntries = $('.cg-glossary-actionable-text');
+
+    let debounce, previousHeight, previousSearchStr;
+
+    glossaryButton.on('click', function(){
+        const activeInd = true,
+            showListResultsInd = true;
+
+        setActiveStatus(glossaryWrapper, activeInd);
+        setTermListView(showListResultsInd);
+    });
+    glossaryWrapper.on('click', '#cg-glossary-close-button', function(){
+        const activeInd = false;
+        setActiveStatus(glossaryWrapper, activeInd);
+    });
+    glossaryWrapper.on('click', '.cg-glossary-link', function(el){
+        const curElement = el.target,
+            termsArr = terms[curElement.getAttribute('hashMap')],
+            termDisplay = curElement.innerText;
+
+        showIndividualTerm(termsArr, termDisplay);
+    });
+    $('.cg-glossary-back').on('click', function(){
+        const showListResultsInd = true;
+        setTermListView(showListResultsInd);
+    });
+
+    searchTextBox.bind("propertychange change click keyup input paste", function(event){
+        if (debounce) {
+            clearTimeout(debounce);
+        }
+
+        const searchStr = $(this).val();
+        const showListResultsInd = true;
+
+        if(previousSearchStr === searchStr){
+            return;
+        }
+
+
+        debounce = setTimeout(function(){
+            createTermHTMLElements(null, searchStr);
+            setTermListView(showListResultsInd);
+        }, 400);
+    });
+    searchForm.on('submit', function(e){
+        const searchStr = searchTextBox.val();
+        const showListResultsInd = true;
+        createTermHTMLElements(null, searchStr);
+        setTermListView(showListResultsInd);
+
+    });
+    actionableTextEntries.on('click', function(){
+        const glossaryTerm = $(this).attr('glossaryTerm'),
+            activeInd = true;
+
+        setActiveStatus(glossaryWrapper, activeInd);
+
+        if(glossaryTerm){
+            showIndividualTerm(glossaryData, glossaryTerm);
+        }
+
+    });
+    window.addEventListener('resize', function(e){
+        if (debounce) {
+            clearTimeout(debounce);
+        }
+
+        if(previousHeight === window.innerHeight){
+            return;
+        }
+        previousHeight = window.innerWidth;
+        debounce = setTimeout(resizeTermListDiv, 100);
+    });
+    setTimeout(function(){
+        resizeTermListDiv();
+    },0);
+}
+
 function init(){
-    const glossaryButton = $('#cg-glossary-btn'),
-     glossaryWrapper = $('#cg-glossary-wrapper'),
-     terms = categorizeGlossaryData();
+    const glossaryWrapper = $('#cg-glossary-wrapper'),
+        glossaryButton = $('#cg-glossary-btn');
 
-
-    let debounce, previousHeight;
+    origDategorizedTerms = categorizeGlossaryData(glossaryData);
 
     if(glossaryButton.length && glossaryWrapper.length){
-        createTermHTMLElements(terms);
-        glossaryButton.on('click', function(){
-            toggleActiveStatus(glossaryWrapper);
-        });
-        glossaryWrapper.on('click', '#cg-glossary-close-button', function(){
-            toggleActiveStatus(glossaryWrapper);
-        });
-        glossaryWrapper.on('click', '.cg-glossary-link', function(el){
-            const curElement = el.target,
-                termDisplay = curElement.innerText;
-
-            let term = terms[curElement.getAttribute('hashMap')].filter(function(d){
-                    return d.term === termDisplay;
-                });
-            if(term.length){
-                const glossaryDefinitionWrapper = '#cg-definition-wrapper',
-                    glossaryDefinitionWrapperDiv = $(glossaryDefinitionWrapper);
-                term = term[0];
-
-                glossaryDefinitionWrapperDiv.empty();
-                showIndividualTerm(glossaryDefinitionWrapperDiv, term);
-                toggleTermListView();
-            }
-        });
-        $('.cg-glossary-back').on('click', function(){
-            toggleTermListView();
-        });
-        window.addEventListener('resize', function(e){
-            if (debounce) {
-                clearTimeout(debounce);
-            }
-
-            if(previousHeight === window.innerHeight){
-                return;
-            }
-            previousHeight = window.innerWidth;
-            debounce = setTimeout(resizeTermListDiv, 100);
-        });
-        setTimeout(function(){
-            resizeTermListDiv();
-        },0);
+        createSVGElements();
+        createTermHTMLElements(origDategorizedTerms);
+        addGlossaryEvents(glossaryWrapper, glossaryButton, origDategorizedTerms);
     }
 }
 
