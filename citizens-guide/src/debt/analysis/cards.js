2c29209eebab1a750fffc9b4ed522f937018d832
Brett Francis <brett.francis@kc.frb.org>
2019-02-19 13:17:23 -0600

    debt cards sizing refinements

--- citizens-guide/src/debt/analysis/cards.js
+++ citizens-guide/src/debt/analysis/cards.js
@@ -1,11 +1,22 @@
 import { select, selectAll } from 'd3-selection';
 
-const d3 = { select, selectAll };
+const d3 = { select, selectAll },
+    flippedClass = 'card--flipped',
+    containerActiveClass = 'cards--flipped',
+    cardContainer = d3.select('.cards'),
+    cards = d3.selectAll('.card');
 
 function toggleState() {
     const card = d3.select(this);
 
-    card.classed('card--flipped', !card.classed('card--flipped'));
+    if (card.classed(flippedClass)) {
+        cardContainer.classed(containerActiveClass, false);
+        cards.classed(flippedClass, false);
+    } else {
+        cards.classed(flippedClass, false);
+        card.classed(flippedClass, true);
+        cardContainer.classed(containerActiveClass, true);
+    }
 }
 
 function buildCover() {
@@ -15,14 +26,10 @@ function buildCover() {
 
     heading.each(function () {
         cover.node().appendChild(this)
-        // ?cover.node().innerHTML = this;
     })
 }
 
 export function initCards() {
-    const cards = d3.selectAll('.card');
-
     cards.each(buildCover);
-
     cards.on('click', toggleState);
 }
