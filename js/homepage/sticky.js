9ee5700e4abd7cec8baf0f0d44ef399f5a4b3f30
598898 <Jack_Killilea@bah.com>
2019-02-27 03:25:27 -0500

    update jquery, fix header bugs + css

--- js/homepage/sticky.js
+++ js/homepage/sticky.js
@@ -1,6 +1,7 @@
 // Jack Killilea (jrk) <Killilea_Jack@bah.com>
 // Sticky.js
-// Quick Sticky Header and Extra Magic for New Homepage!
+// Sticky Header and Extra Magic for New Homepage!
+// using jquery 1.11.3! (updating, if bugged then revert!)
 
 const headerRight = '.header-right';
 const spendingLogo = '.usaspending-logo-center';
@@ -12,6 +13,10 @@ const resourcesNav = '.secondaryNavResources';
 const ffgNav = '.secondaryNavFFG';
 const datalabLogo = '.datalabLogo';
 const burgerMenu = '#burger-navbar-toggle';
+
+// colors!
+const lb = '#2376d8'; // light blue link
+const db = '#0a2644'; // dark blue link
 
 // Main Header Li's for hover
 const analysesText = '.analysesText';
@@ -29,18 +34,15 @@ const mobileAnalyses = '#mobileAnalyses';
 const mobileResources = '#mobileResources';
 const mobileFFG = '#mobileFFG';
 
-// TODOs! //
-// * add dropshadow! 
-// * bring up at retro that mobile mocks and desktop mocks should be the same week!
-// * chat with lizzy about bootstrap ! (whatever)
-
 $(function() {
   stickyHeader($("#header")); // our Header id in our homepage
+  mobileEvents(); // mobile events seperated
+  applyColorsClick(); // added colors 
 
   // on burger menu click, we'll show nav
   $(burgerMenu).click(function() {
-    console.log('clicking burger! Menu will display now!');
-    $(mobileMenu).css('display', 'flex');
+//    $(mobileMenu).css('display', 'flex');
+    $(mobileMenu).toggle('slow');
   });
 
   /* on Nav Li's hover event, show dropdown hidden Ul nav*/
@@ -69,40 +71,46 @@ $(function() {
 
   $(hiddenUlSection).mouseenter(function(){
     $(hiddenUlSection).css('display', 'flex');
-    console.log('entering ul section');
   });
 
   $(hiddenUlSection).mouseleave(function(){
     $(hiddenUlSection).css('display', 'none');
-    console.log('leaving ul section');
   });
+
+});
+
+/*
+  Splitting mobile events into 
+  another function. Just to save
+  someone a headache.
+*/
+function mobileEvents() {
 
   // *** MOBILE *** ///
   $(analysesAnchor).click(function() {
-    console.log('mobile analyses comes into view');
-    $(mobileAnalyses).css('display', 'block');
+//    $(mobileAnalyses).css('display', 'block');
+    $(mobileAnalyses).toggle('slow');
   });
 
   $(resourcesAnchor).click(function() {
-    console.log('mobile resources comes into view');
-    $(mobileResources).css('display', 'block');
+//    $(mobileResources).css('display', 'block');
+    $(mobileResources).toggle('slow');
   });
 
   $(ffgAnchor).click(function() {
-    console.log('mobile ffg comes into view');
-    $(mobileFFG).css('display', 'block');
+//    $(mobileFFG).css('display', 'block');
+    $(mobileFFG).toggle('slow');
   });
 
-  
+}
 
-});
 
 /*
   Check if we're at the top of the page,
   if we are, keep things non sticky,
   when we scroll, lets make things sticky ~
 
-  TODO: Add Scroll event debounce! 
+  TODO: Add Scroll event debounce!
 */
 function stickyHeader(sticky) {
   if (typeof sticky !== "undefined") {
@@ -114,6 +122,14 @@ function stickyHeader(sticky) {
 
         $(burgerMenu).css('padding-right', '0px'); // get rid of burger menu padding..
         $(mobileMenu).css('justify-content','flex-end').css('flex-direction', 'row').css('z-index', '999').css('top', '').css('position', '').css('width', ''); // take defaults from what we have in 'header.css'
+
+        $(spendingLogo).css('justify-content', 'flex-end').css('padding-right', '120px');
+        if($(window).width() >= 1920) {
+          $(spendingLogo).css('justify-content', 'flex-end').css('padding-right', '220px');
+        }
+        if ($(window).width() <= 950) {
+          $(spendingLogo).css('justify-content', 'flex-start').css('padding-right', '0px');
+        }
 
       } else {
 
@@ -127,12 +143,15 @@ function stickyHeader(sticky) {
         // only if we are on the homepage, move logo to left!
         if(!regex.test(pathname)) {
           $(spendingLogo).css('justify-content', 'flex-start').css('padding-right', '0px');
-          console.log('should move to left!');
-        } else {
-          console.log('whatever, shouldnt move to left');
-        }
+        } 
       }
 
-    });
+    }); // end of scroll event !
   }
+}
+
+function applyColorsClick() {
+  $(analysesAnchor).click(function() {
+    $(analysesAnchor).css('color', db);
+  });
 }
