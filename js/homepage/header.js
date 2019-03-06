c6e74aeea4173c6368e53dd6c65e2b0f51d5fabd
598898 <Jack_Killilea@bah.com>
2019-03-06 14:37:48 -0500

    logo resize fixies

--- js/homepage/header.js
+++ js/homepage/header.js
@@ -1,13 +1,16 @@
 // jrk
 
 const headerContainers = {};
+const desktopMin = 956;
+let logoMarginOffset = 149;
 
 function setContainers() {
   headerContainers.header = $('#header');
   headerContainers.headerLogo = $('.header-logo');
+  headerContainers.masterLogo = $('#master-logo');
 }
 
-function fixNav(y) {
+function fixNav(y, width) {
   const max = 29;
   let yOffset = max - y;
   if (y > max) {
@@ -17,15 +20,29 @@ function fixNav(y) {
 
 }
 
-function moveLogo(y) {
+function forceLogoLeft() {
+  headerContainers.headerLogo.css('left', '0%').css('margin-left', '0px');
+}
+
+function moveLogo(y, width) {
   const max = 200;
   let p = 50;
   let steps = p / max; // .25 
   let leftPos = (max - y) * steps;
 
-  let m = 149;
-  let mSteps = m / max;
+  let mSteps = -logoMarginOffset / max;
+
   let leftMargin = 0 - (max - y) * mSteps;
+
+  if (width > desktopMin && width < 1350) {
+    forceLogoLeft();
+    return;
+  }
+
+  if (width < desktopMin) {
+    headerContainers.headerLogo.css('left', '50%').css('margin-left', logoMarginOffset + 'px');
+    return;
+  }
 
   leftPos = leftPos < 0 ? 0 : leftPos;
   leftMargin = leftMargin > 0 ? 0 : leftMargin;
@@ -34,15 +51,15 @@ function moveLogo(y) {
 
 }
 
-function setHeaderOpacity(y) {
+function setHeaderOpacity(y, width) {
   const low = 250;
   const high = 300;
   const range = high - low;
 
   let opacity = 1;
   let yOffset = y - low;
-
-  if (y >= low) {
+  
+  if (y >= low && width >= desktopMin) {
     opacity = 1 - (yOffset / range);
     opacity = opacity < 0 ? 0 : opacity;
   }
@@ -55,11 +72,26 @@ function repositionHeaderItems() {
   
   window.addEventListener('scroll', function() {
     let y = window.scrollY;
-    fixNav(y);
-    moveLogo(y);
-    setHeaderOpacity(y);
+    let width = window.innerWidth;
+    fixNav(y, width);
+    moveLogo(y, width);
+    setHeaderOpacity(y, width);
   });
+
+  window.addEventListener('resize', setInitialLogoPosition);
   
+    
+}
+
+function setInitialLogoPosition() {
+  let width = window.innerWidth;
+  if (width > desktopMin && width < 1350) {
+    forceLogoLeft();
+  } else {
+    logoMarginOffset = 0 - (headerContainers.masterLogo.width() / 2);
+    headerContainers.headerLogo.css('margin-left', logoMarginOffset + 'px').css('left', '50%');
+  }
 }
 
 $(repositionHeaderItems);
+$(setInitialLogoPosition);
