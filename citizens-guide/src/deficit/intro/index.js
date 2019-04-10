e5889ac03912ae2ef4b33ae7758ccdc269f46d8f
Brett Francis <brett.francis@kc.frb.org>
2019-04-10 12:49:31 -0500

    nix redundant polyfill

--- citizens-guide/src/deficit/intro/index.js
+++ citizens-guide/src/deficit/intro/index.js
@@ -8,23 +8,6 @@ import { layersInit, resetLayers } from "./manageLayers";
 import '../../introFactBox';
 import '../../matchesPolyfill';
 
-if (!Element.prototype.matches) {
-    Element.prototype.matches = Element.prototype.msMatchesSelector ||
-        Element.prototype.webkitMatchesSelector;
-}
-
-if (!Element.prototype.closest) {
-    Element.prototype.closest = function (s) {
-        var el = this;
-
-        do {
-            if (el.matches(s)) return el;
-            el = el.parentElement || el.parentNode;
-        } while (el !== null && el.nodeType === 1);
-        return null;
-    };
-}
-
 const config = {
     anecdoteName: 'anecdote-deficit.svg',
     revenueAmount: 3300000000000,
