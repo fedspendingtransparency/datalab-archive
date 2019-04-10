77d444de8a2ad71de32eec44b0c5c7ff1e75578b
Brett Francis <brett.francis@kc.frb.org>
2019-04-10 12:44:58 -0500

    object.matches polyfill

--- citizens-guide/src/deficit/intro/index.js
+++ citizens-guide/src/deficit/intro/index.js
@@ -6,6 +6,24 @@ import colors from '../../colors.scss';
 import { setDotsPerRow } from "./dotConstants";
 import { layersInit, resetLayers } from "./manageLayers";
 import '../../introFactBox';
+import '../../matchesPolyfill';
+
+if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.msMatchesSelector ||
+        Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+    Element.prototype.closest = function (s) {
+        var el = this;
+
+        do {
+            if (el.matches(s)) return el;
+            el = el.parentElement || el.parentNode;
+        } while (el !== null && el.nodeType === 1);
+        return null;
+    };
+}
 
 const config = {
     anecdoteName: 'anecdote-deficit.svg',
