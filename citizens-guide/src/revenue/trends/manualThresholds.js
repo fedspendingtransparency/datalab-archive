468fbfaffc74f9e00ba3b34ef18356b547c1e972
Brett Francis <brett.francis@kc.frb.org>
2019-04-12 15:20:03 -0500

    thresholds for revenue trends

---
+++ citizens-guide/src/revenue/trends/manualThresholds.js
@@ -0,0 +1,6 @@
+export const manualThresholds = {
+    'Corporation Income Taxes': 1,
+    'Excise Taxes': 5000000000,
+    'Unemployment Insurance': 500000000,
+    'Employment and General Retirement': 40000000
+};
