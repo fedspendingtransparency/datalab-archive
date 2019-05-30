2ac721d39a39ed13f0ea7ae27ea11bd16b7f6819
Brett Francis <brett.francis@kc.frb.org>
2019-05-30 10:46:13 -0500

    refactor search to handle new data structure

--- assets/js/colleges-and-universities/agenciesBubbleSearch.js
+++ assets/js/colleges-and-universities/agenciesBubbleSearch.js
@@ -25,7 +25,7 @@
     function filterData() {
         const filterValue = input.property('value').toLowerCase();
 
-        displayList(bubble.agencies.filter(agency => agency.name.toLowerCase().indexOf(filterValue) !== -1));
+        displayList(searchData.filter(agency => agency.name.toLowerCase().indexOf(filterValue) !== -1));
     }
 
     function initInput() {
