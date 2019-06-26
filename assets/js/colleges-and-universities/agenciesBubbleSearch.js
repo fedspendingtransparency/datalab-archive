aa10d45af8625c52d3772d2b943a35d541df3eb9
xjackk <xkillilea@gmail.com>
2019-06-26 15:53:07 -0400

    bubble mobile fixes with search working

--- assets/js/colleges-and-universities/agenciesBubbleSearch.js
+++ assets/js/colleges-and-universities/agenciesBubbleSearch.js
@@ -2,115 +2,185 @@
 ---
 
 (function () {
-    const searchData = [];
+  const searchData = [];
+  
+  let parentSection, searchContainer, inputWrapper, input, list, mobileList, mobileSearchContainer, mobileInputWrapper, mobileInput;
+  let buttons = d3.select('#bubble-function-buttons');
+
+  function initDom() {
+    parentSection = d3.select('#bubble-search');
+    mobileSearchContainer = d3.select('#mobile-search--bubble');
+    searchContainer = parentSection.append('div').classed('bubble-search', true);
+    inputWrapper = searchContainer.append('div').classed('bubble-search__input-wrapper', true);
+    mobileInputWrapper = mobileSearchContainer.append('div').classed('bubble-search__input-wrapper', true);
+    list = searchContainer.append('ul').style('height', (bubble.chartHeight * .7) + 'px').classed('bubble-search__list', true);
+    mobileList = mobileSearchContainer.append('ul').classed('bubble-search__list--mobile', true);
+  }
+
+  function filterFn(row) {
+    if (row.name.toLowerCase().indexOf(this) !== -1) {
+      return true;
+    }
+
+    if (row.parent.name.toLowerCase().indexOf(this) !== -1) {
+      return true;
+    }
+
+    return;
+  }
+
+  function filterData() {
+    const filterValue = input.property('value').toLowerCase();
+    const mobilefilterVal = mobileInput.property('value').toLowerCase();
+
+    displayList(searchData.filter(filterFn.bind(filterValue)));
+    displayMobileList(searchData.filter(filterFn.bind(mobilefilterVal)));
+  }
+
+  function initInput() {
+    input = inputWrapper
+      .append('input')
+      .classed('bubble-search__input', true)
+      .attr('placeholder', 'Search Agencies...')
+      .on('input', filterData);
+
+    mobileInput = mobileInputWrapper
+      .append('input')
+      .classed('bubble-search__input', true)
+      .attr('placeholder', 'Search Agencies...')
+      .on('input', filterData);
+  }
+
+  function initSearch() {
+    initDom();
+    initInput();
+  }
+
+  function selectItem(d) {
+    if (d.zeroLength) {
+      return;
+    }
+
+    bubble.selectSubAgency(d);
+  }
+
+  function prependParent(d) {
+    d3.select(this)
+      .append('span')
+      .text(d => {if (d.parent) return d.parent.name })
+      .classed('bubble-search__parent-name', true);
+  }
+
+  function displayMobileList(filtered) {
+    mobileList.selectAll('li').remove();
     
-    let parentSection, searchContainer, inputWrapper, input, list;
-    let buttons = d3.select('#bubble-function-buttons');
-
-    function initDom() {
-        parentSection = d3.select('#bubble-search');
-        searchContainer = parentSection.append('div').classed('bubble-search', true);
-        inputWrapper = searchContainer.append('div').classed('bubble-search__input-wrapper', true);
-        list = searchContainer.append('ul').style('height', (bubble.chartHeight * .7) + 'px').classed('bubble-search__list', true);
+    if (!filtered.length) {
+      filtered.push({
+        name: 'no items matched your search',
+        zeroLength: true
+      });
     }
 
-    function filterFn(row) {
-        if (row.name.toLowerCase().indexOf(this) !== -1) {
-            return true;
-        }
+    mobileList.selectAll('li')
+      .data(filtered)
+      .enter()
+      .append('li')
+      .classed('bubble-search__item', true)
+      .each(prependParent)
+      .on('click', selectItem)
+      .append('span')
+      .text(d => d.name);
+  };
 
-        if (row.parent.name.toLowerCase().indexOf(this) !== -1) {
-            return true;
-        }
+  function displayList(filtered) {
+    list.selectAll('li').remove();
 
-        return;
+    if (!filtered.length) {
+      filtered.push({
+        name: 'no items matched your search',
+        zeroLength: true
+      });
     }
 
-    function filterData() {
-        const filterValue = input.property('value').toLowerCase();
+    list.selectAll('li')
+      .data(filtered)
+      .enter()
+      .append('li')
+      .classed('bubble-search__item', true)
+      .each(prependParent)
+      .on('click', selectItem)
+      .append('span')
+      .text(d => d.name);
+  }
 
-        displayList(searchData.filter(filterFn.bind(filterValue)));
-    }
+  function flattenData(data) {
+    data.children.forEach(child => {
 
-    function initInput() {
-        input = inputWrapper
-            .append('input')
-            .classed('bubble-search__input', true)
-            .attr('placeholder', 'Search Agencies')
-            .on('input', filterData)
-    }
+      if (child.depth !== 1) {
+        searchData.push(child);
+      };
 
-    function initSearch() {
-        initDom();
-        initInput();
-    }
+      if (child.children && child.children.length >= 1) {
+        flattenData(child);
+      }
+    });
 
-    function selectItem(d) {
-        if (d.zeroLength) {
-            return;
-        }
+    searchData.sort((a, b) => {
+      return a.name - b.name;
+    });
+  }
 
-        bubble.selectSubAgency(d);
-    }
+  function setSearchData(data) {
+    flattenData(data);
 
-    function prependParent(d) {
-        d3.select(this)
-            .append('span')
-            .text(d => {if (d.parent) return d.parent.name})
-            .classed('bubble-search__parent-name', true)
-    }
+    searchData.sort((a, b) => {
+      if (a.name < b.name) { return -1; }
+      if (a.name > b.name) { return 1; }
+      return 0;
+    });
 
-    function displayList(filtered) {
-        list.selectAll('li').remove();
+    initSearch();
+    displayList(searchData);
+    displayMobileList(searchData);
+  }
 
-        if (!filtered.length) {
-            filtered.push({
-                name: 'no items matched your search',
-                zeroLength: true
-            })
-        }
+  $(document).ready(function() {
+    $('.bubble-search__input-wrapper').click(function(){
+      console.log('bubble keydown');
+      $('#mobile-search--bubble ul').css('display','block');
+    });
 
-        list.selectAll('li')
-            .data(filtered)
-            .enter()
-            .append('li')
-            .classed('bubble-search__item', true)
-            .each(prependParent)
-            .on('click', selectItem)
-            .append('span')
-            .text(d => d.name)
-    }
+    // $('#bubble-keydown').focus(function() {
+    //   $('#mobile-search--bubble ul').css('display', 'block');
+    // });
 
-    function flattenData(data) {
-        data.children.forEach(child => {
+    // hide on "clickout" of element
+    $(document).click(function (e) {
+      if ($(e.target).parents("#mobile-search--bubble").length === 0) {
+	$("#mobile-search--bubble ul").css('display', 'none');
+      }
+    });
 
-            if (child.depth !== 1) {
-                searchData.push(child);
-            };
+  });
 
-            if (child.children && child.children.length >= 1) {
-                flattenData(child)
-            }
-            
-        })
+  function searchMobileToggle() {
+    
+    $('.bubble-search__input-wrapper').click(function(){
+      $('.bubble-search-ul--mobile').addClass('active-bubble');
+    });
 
-        searchData.sort((a, b) => {
-            return a.name - b.name;
-        })
-    }
+    $('#bubble-keydown').focus(function() {
+      console.log('bubble keydown');
+      $('.bubble-search-ul--mobile').addClass('active-bubble');
+    });
 
-    function setSearchData(data) {
-        flattenData(data)
+    // hide on "clickout" of element
+    $(document).click(function (e) {
+      if ($(e.target).parents("#mobile-search--bubble").length === 0) {
+	$(".bubble-search-ul--mobile").removeClass('active-bubble');
+      }
+    });
+  };
 
-        searchData.sort((a, b) => {
-            if (a.name < b.name) { return -1; }
-            if (a.name > b.name) { return 1; }
-            return 0;
-        })
-
-        initSearch();
-        displayList(searchData);
-    }
-
-    bubble.setSearchData = setSearchData;
-})()
+  bubble.setSearchData = setSearchData;
+})();
