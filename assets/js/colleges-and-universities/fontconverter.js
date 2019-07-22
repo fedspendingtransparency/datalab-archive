01b097e896775ea1655bb836190dbfbe45014dcb
Andrea Blackwell <blackwell_andrea@bah.com>
2019-07-22 12:05:25 -0400

    font awesome converting i tags to svg

---
+++ assets/js/colleges-and-universities/fontconverter.js
@@ -0,0 +1,13 @@
+---
+---
+
+
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons'
+
+// We are only using the user-astronaut icon
+library.add(faUserAstronaut);
+
+// Replace any existing <i> tags with <svg> and set up a MutationObserver to
+// continue doing this as the DOM changes.
+dom.watch();
