13125156ccabecc22cb9ec0957ca20d46e01be05
Brett Varney <brett.varney@kc.frb.org>
2019-08-09 14:07:51 -0500

    wip

--- assets/rd/spending-chart-generator.js
+++ assets/rd/spending-chart-generator.js
@@ -39,18 +39,16 @@ window.onload = () => {
 			.attr("y", 30)
 			.attr("dy", "-0.55em")
 			.attr("transform", "rotate(90)")
-			.text(d => d.abbreviation)
 			;
 
 		svg.append("g")
 			.attr("class", "y axis")
 			.call(yAxis)
-			.append("text")
-			// .attr("transform", "rotate(-90)")
-			.attr("y", 5)
-			.attr("dy", "0.8em")
-			.attr("text-anchor", "end")
-			.text(d => d.total)
+			// .append("text")
+			// // .attr("transform", "rotate(-90)")
+			// .attr("y", 5)
+			// .attr("dy", "0.8em")
+			// .attr("text-anchor", "end")
 			;
 
 		svg.selectAll("bar")
@@ -58,7 +56,7 @@ window.onload = () => {
 			.enter()
 			.append("rect")
 			.classed("total", true)
-			.attr("x", d => x(d.agency))
+			.attr("x", d => x(d.abbreviation))
 			.attr("width", x.bandwidth())
 			.attr("y", d => y(d.total))
 			.attr("height", d => height - y(d.total))
@@ -87,78 +85,3 @@ window.onload = () => {
 			;
 	});
 }
-
-
-
-
-
-// const margin = {
-// 	top: 10,
-// 	right: 80,
-// 	bottom: 110,
-// 	left: 90
-// };
-// const width = +svg.attr("width") - margin.left - margin.right;
-// const height = +svg.attr("height") - margin.top - margin.bottom;
-
-// const width = 1000;
-// const height = 1000;
-// const x = d3
-// 	.scaleBand()
-// 	.rangeRound([0, width])
-// 	.padding(0.1)
-// ;
-// const y = d3.scaleLinear().rangeRound([height, 0]);
-
-// const groupedData = Object.values(
-// 	data.reduce((a, c) => {
-// 			if (!a[c.occupationCategoryId]) {
-// 					a[c.occupationCategoryId] = {
-// 							occupationCategoryId: c.occupationCategoryId,
-// 							occupationCategoryName:
-// 		occupationCategories[c.occupationCategoryId].shortenedName,
-// 							employeeCount: c.employeeCount,
-// 							occupationCategoryType: c.occupationType
-// 					};
-// 			}
-// 			else {
-// 					a[c.occupationCategoryId].employeeCount += c.employeeCount;
-// 			}
-// 			return a;
-// 	}, {})
-// )
-// 	.sort((a, b) => b.employeeCount - a.employeeCount)
-// 	.reduce((a, c, i) => {
-// 			if (i < 25) {
-// 					a.push(c);
-// 			}
-// 			else if (i === 25) {
-// 					a.push({
-// 							occupationCategoryId: 99,
-// 							occupationCategoryName: "All Other Occupations",
-// 							occupationCategoryType: "Other",
-// 							employeeCount: c.employeeCount
-// 					});
-// 			}
-// 			else if (i > 25) {
-// 					a[25].employeeCount += c.employeeCount;
-// 			}
-// 			return a;
-// 	}, []);
-
-// x.domain(groupedData.map((d) => d.occupationCategoryName));
-// y.domain([0, d3.max(groupedData, (d) => d.employeeCount)]);
-
-// svg.append("g")
-// 	.data(groupedData)
-// 	.enter()
-// 	.append("rect")
-// 	.attr("class", "bar")
-// 	.attr("x", (d) => x(d.occupationCategoryName))
-// 	.attr("y", (d) => y(d.employeeCount))
-// 	.attr("width", x.bandwidth())
-// 	.attr("height", (d) => height - y(d.employeeCount))
-// 	.attr("fill", (d) => findFillColor(d.occupationCategoryType))
-// 	.attr("stroke", "rgb(0,0,0)")
-// 	.attr("stroke-width", "1")
-// ;
