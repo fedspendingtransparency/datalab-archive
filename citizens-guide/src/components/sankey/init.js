c2544b61e9a10a4928aa09779fa369291ee9ae9a
Petway <J1WBP02@rb.win.frb.org>
2019-08-13 15:42:40 -0500

    cg1542 adding consoles for troubleshooting

--- citizens-guide/src/components/sankey/init.js
+++ citizens-guide/src/components/sankey/init.js
@@ -40,9 +40,13 @@ function stackData(series) {
 
         r.x0 = tracker;
 
+        // comment here, just to make the IDE mark the file as changed
+        console.log('r.amount', r.amount);
+        console.log('c.amount', c.amount);
         tracker += Math.abs(r.amount);
 
         if (r.subcategories) {
+            console.log('r.subcategories', r.subcategories);
             stackData(r.subcategories)
         }
 
