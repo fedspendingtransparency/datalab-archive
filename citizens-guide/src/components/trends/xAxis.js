154c1193a7bcc66113e4c57c8bffd1ac94c53cf5
Petway <J1WBP02@rb.win.frb.org>
2019-09-04 16:21:55 -0500

    cg1577 made the trend charts take their fiscal year values dynamically from the csv

--- citizens-guide/src/components/trends/xAxis.js
+++ citizens-guide/src/components/trends/xAxis.js
@@ -4,16 +4,21 @@ import { scaleLinear } from 'd3-scale';
 const d3 = { axisBottom, scaleLinear }
 
 function init(globals) {
+    const fiscalYearArraylength = globals.fiscalYearArray.length;
+    const first = globals.fiscalYearArray[0];
+    const last = globals.fiscalYearArray[fiscalYearArraylength - 1];
+
     globals.scales.x = d3.scaleLinear()
         .range([0, globals.width])
-        .domain([2014, 2018]);
+        .domain([first, last]);
 }
 
 function render(globals) {
     const x = this;
     
     x.xAxis = d3.axisBottom(globals.scales.x)
-        .tickValues([2014, 2015, 2016, 2017, 2018])
+        // .tickValues([2014, 2015, 2016, 2017, 2018])
+        .tickValues(globals.fiscalYearArray)
         .tickFormat(t => parseInt(t))
 
     x.xAxisDom = globals.chart.append('g')
