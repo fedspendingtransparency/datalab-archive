65e2309c49fbfe333b9f5579aa5f3333557bf142
Petway <J1WBP02@rb.win.frb.org>
2019-09-30 13:36:57 -0500

    cg1578 updated accessibility attributes with yml variables where possible, still a handful of places where it will have to be done manually when new data is available

--- citizens-guide/src/debt/countries/index.js
+++ citizens-guide/src/debt/countries/index.js
@@ -3,6 +3,7 @@ import { loadSourceData } from '../../revenue/countries/data';
 import CountryData from '../../../../assets/ffg/data/debt_country_comparison.csv';
 import { chartInit } from '../../revenue/countries/chart';
 import colors from '../../globalSass/colors.scss';
+import Mapping from '../../../../_data/object_mapping.yml';
 
 const spendingConfig = {
     amountField: 'debt_usd',
@@ -35,7 +36,7 @@ const spendingConfig = {
     }],
     accessibilityAttrs : {
         title: 'Federal Debt Country Comparison',
-        desc: 'By the end of 2017, the five largest countries in terms of federal revenue and spending had the following government debt outstanding: the United States with $20.2 trillion (104% of gross domestic product), Japan with $11.6 trillion (239%), China with $5.8 trillion (48%), France with $2.6 trillion (101%), and Germany with $2.5 trillion (68%).'
+        desc: `By the end of ${Mapping.country_compare_year.value}, the five largest countries in terms of federal revenue and spending had the following government debt outstanding: the United States with ${Mapping.compare_us_debt.value} (${Mapping.compare_us_debt_gdp.value} of gross domestic product), Japan with $11.6 trillion (239%), China with $5.8 trillion (48%), France with $2.6 trillion (101%), and Germany with $2.5 trillion (68%).`
     }
 };
 
--- citizens-guide/src/revenue/intro/index.js
+++ citizens-guide/src/revenue/intro/index.js
@@ -2,6 +2,7 @@ import { initChart } from '../../spending/intro/init';
 import colors from '../../globalSass/colors.scss';
 import revenueData from '../../../../assets/ffg/data/federal_revenue_gdp.csv';
 import { findAmountInCsv } from '../../../src/utils';
+import Mapping from "../../../../_data/object_mapping.yml";
 
 (function init(){
     const config = {
@@ -15,16 +16,16 @@ import { findAmountInCsv } from '../../../src/utils';
         sectionColor: colors.revenuePrimary,
         accessibilityAttrs : {
             default: {
-                title: '2018 Federal Revenue',
-                desc: 'The image illustrates federal revenue in 2018 using dots, and each dot is equal to a billion dollars. There are 3,300 dots.'
+                title: `${Mapping.current_fy.value} Federal Revenue`,
+                desc: `The image illustrates federal revenue in ${Mapping.current_fy.value} using dots, and each dot is equal to ${Mapping.dot_represents.value}. There are ${Mapping.dot_number_revenue.value}dots.`
             },
             gdp : {
-                title: '2018 Federal Revenue and GDP',
-                desc: 'The U.S. economy, as measured by gross domestic product, produced $20.7 trillion worth of goods and services. In 2018, federal revenue was equivalent to 16% of gross domestic product.'
+                title: `${Mapping.current_fy.value} Federal Revenue and GDP`,
+                desc: `The U.S. economy, as measured by gross domestic product, produced ${Mapping.current_fy_gdp.value} worth of goods and services. In ${Mapping.current_fy.value}, federal revenue was equivalent to ${Mapping.revenue_percent_gdp.value} of gross domestic product.`
             },
             spending : {
-                title: '2018 Federal Revenue and Spending',
-                desc: 'The image illustrates federal spending in 2018 using dots, and each dot is equal to a billion dollars. There are 4,100 dots. That means there are 800 more spending dots than revenue dots, representing the deficit for 2018, roughly $800 billion ($779B).'
+                title: `${Mapping.current_fy.value} Federal Revenue and Spending`,
+                desc: `The image illustrates federal spending in ${Mapping.current_fy.value} using dots, and each dot is equal to ${Mapping.dot_represents.value}. There are ${Mapping.dot_number_spending.value} dots. That means there are 800 more spending dots than revenue dots, representing the deficit for ${Mapping.current_fy.value}, roughly $800 billion ($779B).`
             }
         }
     };
