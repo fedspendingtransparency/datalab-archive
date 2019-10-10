f4a06fb3922ba915677691db497794455204ae80
petway <brian.petway@kc.frb.org>
2019-10-10 17:00:58 -0400

    cg1815 seo updates for 2019

--- citizens-guide/src/revenue/categories/index.js
+++ citizens-guide/src/revenue/categories/index.js
@@ -12,7 +12,7 @@ const config = {
     containerClass: receiptsConstants.shaderContainerClass,
     sectionColor: colors.colorPrimaryDarker,
     accessibilityAttrs: {
-        title: `${Mapping.current_fy.value} Federal Revenue Categories`,
+        title: '2019 Federal Revenue Categories',
         desc: `The federal government collected $1.7 trillion in individual income taxes in ${Mapping.current_fy.value}. That represented 51% of all federal revenue for the year. Social Security and Medicare taxes added another $1.1 trillion (or 34%) of total federal revenue. Corporate income taxes were the third largest source of revenue for the federal government in 2018 with $205 billion collected. Miscellaneous revenue, excise taxes, unemployment insurance, customs duties, estate and gift taxes, and other retirement taxes contributed the remaining $325 billion of federal revenue for ${Mapping.current_fy.value}.`
     }
 },
